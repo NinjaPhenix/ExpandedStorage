dd84901ba582c338d5d42705a714c397c3954d1a
NinjaPhenix <5214513+NinjaPhenix@users.noreply.github.com>
2021-07-11 21:55:07 +0100

    Move forge specific classes to the forge subproject

--- forge/src/old_chest/java/ninjaphenix/expandedstorage/old_chest/block/OldChestBlock.java
+++ forge/src/old_chest/java/ninjaphenix/expandedstorage/old_chest/block/OldChestBlock.java
@@ -9,7 +9,6 @@ import ninjaphenix.expandedstorage.base.internal_api.block.AbstractChestBlock;
 import ninjaphenix.expandedstorage.old_chest.OldChestCommon;
 import ninjaphenix.expandedstorage.old_chest.block.misc.OldChestBlockEntity;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 public final class OldChestBlock extends AbstractChestBlock<OldChestBlockEntity> {
     public OldChestBlock(Properties properties, ResourceLocation blockId, ResourceLocation blockTier,
